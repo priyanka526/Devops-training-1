fe074e8252b5d47d946f7fc9cf553262c2fe2a36
priyanka526 <66724273+priyanka526@users.noreply.github.com>
2020-06-11 16:31:39 +0530

    phython-install

--- git_jenkins_test1.sh
+++ git_jenkins_test1.sh
@@ -3,4 +3,8 @@ echo "This is to test Int"
 Echo "I am editing to check changes in jenkins"
 echo "I am updating this file agian to see changes in jenkins"
 yum install -y maven
-echo "hi"
+echo "=============================="
+yum install -y https://repo.ius.io/ius-release-el7.rpm
+yum update -y
+yum install -y python36u python36u-libs python36u-devel python36u-pip
+exit 0
