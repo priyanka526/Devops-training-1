447310ad147204832a5a28c8c769c1c893dfda01
priyanka526 <66724273+priyanka526@users.noreply.github.com>
2020-06-11 16:38:50 +0530

    artficate_file

--- git_jenkins_test1.sh
+++ git_jenkins_test1.sh
@@ -7,4 +7,5 @@ echo "=============================="
 yum install -y https://repo.ius.io/ius-release-el7.rpm
 yum update -y
 yum install -y python36u python36u-libs python36u-devel python36u-pip
+date >> demo.txt
 exit 0
