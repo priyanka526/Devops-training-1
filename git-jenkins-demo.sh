5316c1f9999ed9047ca482aeb2b5a186624cf8e2
priyanka526 <66724273+priyanka526@users.noreply.github.com>
2020-06-12 16:04:05 +0530

    Update message

--- git-jenkins-demo.sh
+++ git-jenkins-demo.sh
@@ -3,6 +3,8 @@ echo "This is to show github and jenkins integration"
 echo "This is show version control in UCD"
 yum install -y ntp httpd
 echo "UCD deployment"
+echo "This Deployment is successful"
 mkdir Priyanka
+
 exit 0
 
